49c8cf4d2d75d9d45a4a19468310090dcead3c21
Stepainpy <stepa2014vlg@gmail.com>
2026-03-08 21:07:09 +0300

    add updated rc4

---
+++ cipher/rc4/rc4.c
@@ -0,0 +1,45 @@
+#include "rc4.h"
+#include "config.h"
+
+typedef stmcphr_u8_t rc4_byte_t;
+
+static struct {
+    rc4_byte_t S[256];
+    rc4_byte_t i, j;
+} rc4i_ctx;
+
+#define rc4i_swap(a, b) do { rc4_byte_t t = (a); (a) = (b); (b) = t; } while (0)
+
+int rc4_setup_key(const void* key, int bits) {
+    const rc4_byte_t* K = key; int i, j;
+
+    if (bits < 40 || bits > 2048) return 1;
+    if (bits % 8 != 0) return 1;
+
+    rc4i_ctx.i = rc4i_ctx.j = 0;
+    for (i     = 0; i < 256; i++) rc4i_ctx.S[i] = i;
+    for (i = j = 0; i < 256; i++) {
+        j = (j + rc4i_ctx.S[i] + K[i % (bits / 8)]) & 255;
+        rc4i_swap(rc4i_ctx.S[i], rc4i_ctx.S[j]);
+    }
+
+    return 0;
+}
+
+void rc4_take_gamma(void* dest, size_t count) {
+    rc4_byte_t* dst = dest;
+    while (count --> 0) {
+        rc4i_ctx.j += rc4i_ctx.S[++rc4i_ctx.i];
+        rc4i_swap(rc4i_ctx.S[rc4i_ctx.i], rc4i_ctx.S[rc4i_ctx.j]);
+        *dst++ = rc4i_ctx.S[(
+            rc4i_ctx.S[rc4i_ctx.i] + rc4i_ctx.S[rc4i_ctx.j]
+        ) & 255];
+    }
+}
+
+void rc4_skip_gamma(size_t count) {
+    while (count --> 0) {
+        rc4i_ctx.j += rc4i_ctx.S[++rc4i_ctx.i];
+        rc4i_swap(rc4i_ctx.S[rc4i_ctx.i], rc4i_ctx.S[rc4i_ctx.j]);
+    }
+}
---
+++ cipher/rc4/rc4.h
@@ -0,0 +1,20 @@
+/* Rivest's cipher 4 (RC4)
+ *
+ * Length of key   - from 40 to 2048 bit
+ * Length of nonce - none
+ * Sorces:
+ *   https://en.wikipedia.org/wiki/RC4
+ *   https://datatracker.ietf.org/doc/html/rfc6229
+ */
+
+#ifndef RC4_STREAM_CIPHER_H
+#define RC4_STREAM_CIPHER_H
+
+#include <stddef.h>
+
+int rc4_setup_key(const void* key, int key_bits);
+
+void rc4_take_gamma(void* dest, size_t count);
+void rc4_skip_gamma(            size_t count);
+
+#endif /* RC4_STREAM_CIPHER_H */
